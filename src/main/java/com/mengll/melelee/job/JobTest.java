9eccfb7e937f4cffc66bd8edf153c863e7660c3a
MeLeeLee <984884301@qq.com>
2018-02-02 15:38:38 +0800

    集成quartz

---
+++ src/main/java/com/mengll/melelee/job/JobTest.java
@@ -0,0 +1,17 @@
+package com.mengll.melelee.job;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Job测试
+ *
+ * @author mengll
+ * @create 2018-01-22 16:35
+ **/
+public class JobTest {
+	private static Logger logger = LoggerFactory.getLogger(JobTest.class);
+	public void start(){
+		logger.info("JOB测试开始执行...");
+	}
+}
